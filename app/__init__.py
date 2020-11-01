3372a10fa99498732a56367c3361a12d1b5eb8f7
Orest Nowosad <nowosad.orest@gmail.com>
2020-11-01 14:35:55 -0500

    Add application blueprints for core and auth apps

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_babel import Babel, lazy_gettext as _l
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -11,8 +12,8 @@ db = SQLAlchemy()
 migrate = Migrate()
 login = LoginManager()
 login.login_view = 'login'
-login.login_message = 'Log in to access this page.'
-socketio = SocketIO(app)
+login.login_message = _l('Log in to access this page.')
+socketio = SocketIO()
 
 
 def create_app(config_class=ProductionConfig):
@@ -30,5 +31,12 @@ def create_app(config_class=ProductionConfig):
 
     login.init_app(app)
     socketio.init_app(app)
+    babel.init_app(app)
+
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp)
+
+    from app.core import bp as core_bp
+    app.register_blueprint(core_bp)
 
     return app
---
+++ app/core/__init__.py
@@ -0,0 +1,7 @@
+from flask import Blueprint
+
+
+bp = Blueprint('core', __name__)
+
+
+from app.core import routes
